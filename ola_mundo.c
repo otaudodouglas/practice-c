76b9f8c4bec16a1aecfb3cddfc81b979385f8781
Douglas luz <dodogame8574@gmail.com>
2024-11-05 16:04:46 -0300

    primeiro commit

--- ola_mundo.c
+++ ola_mundo.c
@@ -2,6 +2,6 @@
 
 int main() {
     printf("Olá, mundo!\n");
-    system("pause")
+
     return 0;
 }
---
+++ soma_numero.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int nro;
+printf("primeiro numero");
+scanf("%d" ,&nro);
+printf("numero digitado:  %d \n",nro);
+
+    return 0;
+}
