6072e405608ff5e1b7dad7cb7fb1a87918bd2e1e
Douglas luz <dodogame8574@gmail.com>
2024-11-05 16:42:45 -0300

    atividade 3

---
+++ soma_numero.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int nro1, nro2, soma;
+
+    printf("digite um numero:\n");
+    scanf("%d", &nro1);
+
+    printf("digite um numero\n");
+    scanf("%d", &nro2);
+
+    soma= nro1 + nro2;
+    printf("o valor e: %d\n", soma);
+
+    return 0;
+}
